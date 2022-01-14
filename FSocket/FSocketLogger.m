c4521486e8cff3835b63f3c9d842c6eeb60e4f8a
krishpranav <krisna.pranav@gmail.com>
2022-01-14 17:12:29 +0530

    FSocket: FSocketLogger

--- FSocket/FSocketLogger.m
+++ FSocket/FSocketLogger.m
@@ -9,23 +9,23 @@
 
 @implementation FSocketLogger
 
-- (instancetype) init {
+- (instancetype)init {
     self = [super init];
     if (self) {
         self.log = NO;
     }
-    
     return self;
 }
 
 -(void) log:(NSString*)message type:(NSString*)type {
+    [self printLog:@"LOG" message:message type:type];
 }
 
 -(void) error:(NSString*)message type:(NSString*)type {
+    [self printLog:@"ERROR" message:message type:type];
 }
 
--(void) printLog:(NSString*)logType message:(NSString*)message type:(NSString*)type
-{
+-(void) printLog:(NSString*)logType message:(NSString*)message type:(NSString*)type {
     if(_log) {
         NSLog(@"%@ %@: %@", logType, type, message);
     }
