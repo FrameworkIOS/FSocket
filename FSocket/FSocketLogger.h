0cefa5dd93b1d6b2e7f8cb4504d66ad09b77eff3
krishpranav <krisna.pranav@gmail.com>
2022-01-14 17:03:20 +0530

    FSocket: FSocketLogger header

---
+++ FSocket/FSocketLogger.h
@@ -0,0 +1,16 @@
+//
+//  FSocketLogger.h
+//  FSocket
+//
+//  Created by Apple on 14/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FSocketLogger:NSObject
+
+@property (nonatomic) BOOL log;
+
+-(void)log: (NSString*)message type:(NSString*)type;
+
+@end
