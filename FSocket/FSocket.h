3eb5092027766257a120b15811ad9ddc11e1870a
krishpranav <krisna.pranav@gmail.com>
2022-01-14 15:11:02 +0530

    add files via upload

---
+++ FSocket/FSocket.h
@@ -0,0 +1,18 @@
+//
+//  FSocket.h
+//  FSocket
+//
+//  Created by Apple on 14/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FSocket.
+FOUNDATION_EXPORT double FSocketVersionNumber;
+
+//! Project version string for FSocket.
+FOUNDATION_EXPORT const unsigned char FSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSocket/PublicHeader.h>
+
+
