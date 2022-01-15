cdbedf46f510278898132d106f5e90a38f0d8830
krishpranav <krisna.pranav@gmail.com>
2022-01-15 11:35:27 +0530

    FSocket: FSocketAckEmitter header

---
+++ FSocket/FSocketAckEmitter.h
@@ -0,0 +1,15 @@
+//
+//  FSocketAckEmitter.h
+//  FSocket
+//
+//  Created by Apple on 15/01/22.
+//
+
+#import <Foundation/Foundation.h>
+#import "FSocketClientProtocol.h"
+
+@interface FSocketAckEmitter : NSObject
+
+-(instancetype)initWithSocket:(id<FSocketClientProtocol>)socket ackNum:(int)ack;
+
+@end
