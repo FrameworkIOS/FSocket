18c0f585196c87418978e3a08a88c8363a3ac1e3
krishpranav <krisna.pranav@gmail.com>
2022-01-15 09:56:06 +0530

    FSocket: DefaultSocketLogger

---
+++ FSocket/DefaultSocketLogger.m
@@ -0,0 +1,18 @@
+//
+//  DefaultSocketLogger.m
+//  FSocket
+//
+//  Created by Apple on 15/01/22.
+//
+
+#import "DefaultSocketLogger.h"
+
+@implementation DefaultSocketLogger
+
+static FSocketLogger *logInstance;
+
++ (void)setLogger:(FSocketLogger *)newLogger {
+    logInstance = newLogger;
+}
+
+@end
