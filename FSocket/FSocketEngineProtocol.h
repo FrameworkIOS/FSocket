2ec89bba92f25135f8e87bfd299bb95458442bbc
krishpranav <krisna.pranav@gmail.com>
2022-01-15 14:38:31 +0530

    FSocket: FSocketEngineProtocol header

--- FSocket/FSocketEngineProtocol.h
+++ FSocket/FSocketEngineProtocol.h
@@ -22,15 +22,21 @@
 
 @end
 
+
 @protocol FSocketEngineProtocol <NSObject>
 
 @required
 
+@property (nonatomic, weak) id<FSocketEngineClient> client;
 @property (nonatomic, readonly) BOOL closed;
 @property (nonatomic, readonly) BOOL connected;
 
 -(void)connect;
+-(void)disconnect:(NSString*)reason;
+-(void)syncResetClient;
+-(void)send:(NSString*)msg withData:(NSArray<NSData*>*) data;
 
 @end
 
-#endif /* FSocketEngineProtocol_h */
+#endif
+
