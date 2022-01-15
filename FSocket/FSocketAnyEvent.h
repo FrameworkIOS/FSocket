c35bc5a01799beb10338f158e2d7d9adcdcee1ec
krishpranav <krisna.pranav@gmail.com>
2022-01-15 14:16:36 +0530

    FSocket: FSocketAnyEvent header

---
+++ FSocket/FSocketAnyEvent.h
@@ -0,0 +1,24 @@
+//
+//  FSocketAnyEvent.h
+//  FSocket
+//
+//  Created by Apple on 15/01/22.
+//
+
+#import <Foundation/Foundation.h>
+#import "FSocketClientProtocol.h"
+
+@interface FSocketAnyEvent : NSObject
+
+@property (nonatomic, strong, readonly) NSString* event;
+@property (nonatomic, strong, readonly) NSArray *items;
+
+-(instancetype)initWithEvent:(NSString*)event andItems:(NSArray*)items;
+
+@end
+
+@interface FSocketEventHandler : NSObject
+
+@property (nonatomic, strong, readonly) NSString* event;
+
+@end
