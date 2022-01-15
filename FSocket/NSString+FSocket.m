673410e03919ffecb90dffd3ba1a8ccb42f551b1
krishpranav <krisna.pranav@gmail.com>
2022-01-15 10:14:32 +0530

    FSocket: NSString+FSocket

--- FSocket/NSString+FSocket.m
+++ FSocket/NSString+FSocket.m
@@ -6,3 +6,34 @@
 //
 
 #import "NSString+FSocket.h"
+
+@implementation NSString (FSocket)
+
+-(NSDictionary*)toDictionary {
+    NSData *binData = [self dataUsingEncoding:NSUnicodeStringEncoding allowLossyConversion:NO];
+    if (binData != nil) {
+        NSError *error = nil;
+        id json = [NSJSONSerialization JSONObjectWithData:binData options:NSJSONReadingAllowFragments error:&error];
+        if(error == nil && [json isKindOfClass:[NSDictionary class]]) {
+            return json;
+        }
+    }
+    
+    return nil;
+}
+
+-(NSString*)urlEncode {
+    return [self stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+}
+
+-(NSArray*)toArray {
+    NSData *binData = [self dataUsingEncoding:NSUnicodeStringEncoding allowLossyConversion:NO];
+    if (binData != nil) {
+        NSError *error = nil;
+    }
+    
+    return nil
+}
+
+
+@end
