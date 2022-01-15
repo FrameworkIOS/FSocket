f8761da08a0766aa8e3ff2c4b9c77fd170c89533
krishpranav <krisna.pranav@gmail.com>
2022-01-15 14:25:32 +0530

    FSocket: FSocketAnyEvent

---
+++ FSocket/FSocketAnyEvent.m
@@ -0,0 +1,42 @@
+//
+//  FSocketAnyEvent.m
+//  FSocket
+//
+//  Created by Apple on 15/01/22.
+//
+
+#import "FSocketAnyEvent.h"
+#import "FSocketAckEmitter.h"
+
+@implementation FSocketAnyEvent
+
+-(NSString *)description {
+    return [NSString stringWithFormat:@"FSocketAnyEvent: Event: %d items: %@", (int)_event, _items.description];
+}
+
+-(instancetype)initWithEvent:(NSString *)event andItems:(NSArray *)items {
+    self = [super init];
+    if (self) {
+        _event = event;
+        _items = items;
+    }
+    
+    return self;
+}
+
+@end
+
+@implementation FSocketEventHandler : NSObject
+
+-(instancetype)initWithEvent:(NSString *)event uuid:(NSUUID *)uuid andCallback:(FSocketOnEventCallback)callback {
+    self = [super init];
+    if(self) {
+        _event = event;
+        _uuid = uuid;
+        _callback = callback;
+    }
+    
+    return self;
+}
+
+@end
