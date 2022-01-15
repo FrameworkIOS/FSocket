3244f7ac3575475be36013d539188ba343dd1ce1
krishpranav <krisna.pranav@gmail.com>
2022-01-15 10:07:44 +0530

    FSocket: NSString+FSocket header

---
+++ FSocket/NSString+FSocket.h
@@ -0,0 +1,15 @@
+//
+//  NSString+FSocket.h
+//  FSocket
+//
+//  Created by Apple on 15/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (FSocket)
+
+-(NSDictionary*)toDictionary;
+-(NSString*)urlEncode;
+
+@end
