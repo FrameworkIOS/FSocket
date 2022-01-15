8f9edeeaa36784b3394fa6accdd06786c5c50869
krishpranav <krisna.pranav@gmail.com>
2022-01-15 14:27:14 +0530

    FSocket: FSocketAnyEvent

--- FSocket/FSocketAnyEvent.m
+++ FSocket/FSocketAnyEvent.m
@@ -14,29 +14,31 @@
     return [NSString stringWithFormat:@"FSocketAnyEvent: Event: %d items: %@", (int)_event, _items.description];
 }
 
--(instancetype)initWithEvent:(NSString *)event andItems:(NSArray *)items {
+-(instancetype)initWithEvent:(NSString*)event andItems:(NSArray*)items {
     self = [super init];
-    if (self) {
+    if(self) {
         _event = event;
         _items = items;
     }
-    
     return self;
 }
 
 @end
 
+
 @implementation FSocketEventHandler : NSObject
 
--(instancetype)initWithEvent:(NSString *)event uuid:(NSUUID *)uuid andCallback:(FSocketOnEventCallback)callback {
+-(instancetype)initWithEvent:(NSString*)event uuid:(NSUUID*)uuid andCallback:(FSocketOnEventCallback)callback{
     self = [super init];
     if(self) {
         _event = event;
         _uuid = uuid;
         _callback = callback;
     }
-    
     return self;
 }
 
+-(void)executeCallbackWith:(NSArray*)items withAck:(int)ack withSocket:(id<FSocketClientProtocol>)socket {
+    self.callback(items, [[FSocketAckEmitter alloc] initWithSocket:socket ackNum:ack]);
+}
 @end
