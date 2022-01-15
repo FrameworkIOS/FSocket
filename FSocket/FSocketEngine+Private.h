09f16b8f4c4b0bbd05e80dd5b3d7c9290f4381f4
krishpranav <krisna.pranav@gmail.com>
2022-01-15 14:33:53 +0530

    FSocket: FSocketEnginePrivate

---
+++ FSocket/FSocketEngine+Private.h
@@ -0,0 +1,17 @@
+//
+//  FSocketEngine+Private.h
+//  FSocket
+//
+//  Created by Apple on 15/01/22.
+//
+
+#import "FSocketEngine.h"
+#import "JFRWebSocket.h"
+
+typedef enum : NSUInteger{
+    FS,cketEnginePacketTypeOpen = 0x0
+}
+
+@interface FSocketEngine ()
+
+@end
---
+++ FSocket/FSocketEngine.h
@@ -0,0 +1,12 @@
+//
+//  FSocketEngine.h
+//  FSocket
+//
+//  Created by Apple on 15/01/22.
+//
+
+#ifndef FSocketEngine_h
+#define FSocketEngine_h
+
+
+#endif /* FSocketEngine_h */
